db9c371cae2789edc7b94fd436e1e8c655c273e7
root <root@srv1489383.hstgr.cloud>
2026-05-21 13:51:06 +0000

    Prevent cached 404 on detail pages

--- src/app/sbm/[slug]/page.tsx
+++ src/app/sbm/[slug]/page.tsx
@@ -2,6 +2,9 @@ import { SbmDetailPage } from "@/components/sbm/sbm-detail-page";
 import { buildPostMetadata, buildTaskMetadata } from "@/lib/seo";
 import { fetchTaskPostBySlug, fetchTaskPosts } from "@/lib/task-data";
 
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export const revalidate = 3;
 
 export async function generateStaticParams() {
